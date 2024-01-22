7dcbbb6e5f4646628e6a0d9279f5150be892e6c1
Natalia Telpukhova <nataliatelpukhova@Natalias-MacBook-Air.local>
2024-01-21 18:17:38 -0800

    Update html and css for initial look

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <form className="note-form">
+        <input placeholder="Title" required />
+        <textarea placeholder="Content" rows={10} required />
+
+        <button type="submit">Add Note</button>
+      </form>
+      <div className="notes-grid">
+        <div className="note-item">
+          <div className="notes-header">
+            <button>x</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Note content</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
